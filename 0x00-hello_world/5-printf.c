598ed455d7a75942186556bcb1c825f5b2e6b36a
Horluwatomee <fehinglobal14@gmail.com>
2022-09-08 21:12:31 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (success)
- * /
- *
+ * main -print the string in the put function
+ * Return: 0
+ */
+ 
  int main(void)
  {
   	printf("with proper grammar, but the outcome is a piece of art,\n");
   	return (0);
  }
- *
