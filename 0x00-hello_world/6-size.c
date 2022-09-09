de043bcb508159fae363f8be1a5f90002f4ba0bc
Horluwatomee <fehinglobal14@gmail.com>
2022-09-08 21:38:18 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the sizes of the type
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %|d byte(s)\n", sizeof(c));
+	printf("Size of an int: %|d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %|d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %|d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %|d byte(s) \n", sizeof(f));
+	return (0);
+}
