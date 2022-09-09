8e9052c88a2dd4e4f963b5d3eb6474bc4138f367
Horluwatomee <fehinglobal14@gmail.com>
2022-09-08 19:23:02 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
-int main(void) {
-  puts("programming is like building a multilingual puzzle\n"); return
-								  (0);}
+/**
+ * Your program should end with the value 0
+ */
+int main(void)
+{
+  puts("programming is like building a multilingual puzzle\n");
+  return
+								  (0);
+}
+
