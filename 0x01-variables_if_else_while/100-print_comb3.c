77920616b3bb13236681676b45004357557afc3a
Horluwatomee <fehinglobal14@gmail.com>
2022-09-11 13:47:23 -0700

    double digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combinations of 2 digits
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int d1;
+
+	int d2;
+
+	for (d1 = 0; d1 <= 9; d1++)
+	{
+		putchar(d1 + '0');
+	}
+	for (d2 = 0; d2 <= 9; d2++)
+	{
+		putchar(d2 + '0');
+
+	if (d1 == 9 && d2 == 9)
+		continue;
+	
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
