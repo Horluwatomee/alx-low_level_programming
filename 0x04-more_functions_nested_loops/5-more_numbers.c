8eb645203bc7bd57c241cdd10eb523a5c7f45a9f
Horluwatomee <fehinglobal14@gmail.com>
2022-09-16 00:58:17 -0700

    print 10 times numbers 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
 	char i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = '0'; j <= 14; j++)
 		{
 			if (j >= 10)
 				_putchar(j / 10);
