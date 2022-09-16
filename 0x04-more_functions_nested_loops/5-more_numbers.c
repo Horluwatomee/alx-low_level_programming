1d8c094c4a64f68058162909015c9b3d6be87be7
Horluwatomee <fehinglobal14@gmail.com>
2022-09-16 00:43:29 -0700

    Print number 0 to 14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,16 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	char i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 15; j++)
 		{
 			if (j >= 10)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+				_putchar(j / 10);
+			_putchar(j % 10);
 		}
 		_putchar('\n');
 	}
 }
-
