a0a397bcd993bf3660256a6b1f5e34bc863729d3
Horluwatomee <fehinglobal14@gmail.com>
2022-09-27 19:06:32 -0700

    search a string for any set of byte

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,20 +10,20 @@
  * or NULL if no such byte is found
  */
 
- char *_strpbrk(char *s, char *accept)
- {
-	 int i, j;
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
 
-	 for (i = 0; *s != '\0'; i++)
-	 {
-		 for (j = 0. accept[j] != '\0'; j++)
-		 {
-			 if (*s == accept[j])
-			 {
-				 return (s);
-			 }
-		 }
-		 s++;
-	 }
-	 return (NULL);
- }
+	for (i = 0; *s != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
