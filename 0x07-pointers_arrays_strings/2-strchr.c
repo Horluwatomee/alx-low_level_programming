e07ee0094087c4b8dbbb352ec7b36446eb5496e4
Horluwatomee <fehinglobal14@gmail.com>
2022-09-27 18:30:39 -0700

    copy initial char to a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,27 +1,28 @@
 #include "main.h"
+#define NULL 0
 
 /**
  * _strchr - function that locates a character in a string.
  * @s: the string
- * @c: the character
+ * @c: the character to be copied
  *
  * Return: pointer to the first character
  */
 
 char *_strchr(char *s, char c)
 {
-	int a;
-
-	while (1)
+	while (*s)
 	{
-		a = *s++
-			if (a == c)
-			{
-				return (s - 1);
-			}
-		if (a == 0)
+		if (*s != c)
+			s++;
+		else
 		{
-			return (NULL);
+			return (s);
 		}
 	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
 }
