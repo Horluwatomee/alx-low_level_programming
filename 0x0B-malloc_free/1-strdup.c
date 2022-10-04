afd46b8a2303bcc9416c4f3c2f974e3f02bba8f0
Horluwatomee <fehinglobal14@gmail.com>
2022-10-04 13:13:46 -0700

    returns pointer to new memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,30 +10,24 @@
 
 char *_strdup(char *str)
 {
-	char *alloc_mem;
+	char *strDup;
 
-	unsigned int i;
-
-	unsigned int length_of_string = 0;
+	int i, j;
 
 	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	i++;
+	strDup = malloc(sizeof(*str) * i);
+	if (strDup == NULL)
+		return (NULL);
+	j = 0;
+	while (str[j] != '\0')
 	{
-		return ('\0');
+		strDup[j] = str[j];
+		j++;
 	}
-	while (*(str + length_of_string) != '\0')
-		length_of_string++;
-	{
-		length_of_string++;
-		alloc_mem = malloc(sizeof(*str) * length_of_string);
-	}
-	if (alloc_mem == NULL)
-	{
-		return ('\0');
-	}
-	for (i = 0; i < length_of_string; i++)
-	{
-		alloc_mem[i] = *(str + i);
-		alloc_mem[i] = '\0';
-	}
-	return (alloc_mem);
+	return (strDup);
 }
