cba2cb4eb56521aec25a96425f2a92a516d89a0d
Horluwatomee <fehinglobal14@gmail.com>
2022-10-04 07:55:33 -0700

    create and init a char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,15 +14,15 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
-	
+
 	unsigned int i;
-	
+
 	if (size == 0)
 	{
 		return ('\0');
 	}
 	arr = malloc(size);
-	
+
 	if (arr == NULL)
 	{
 		return ('\0');
