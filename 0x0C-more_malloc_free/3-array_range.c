6d9ce2a824cb123c62acf1d73e8171ed397a36cb
Horluwatomee <fehinglobal14@gmail.com>
2022-10-07 08:08:15 -0700

    allocates memory for an array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,5 +13,19 @@
 
 int *array_range(int min, int max)
 {
+	int i, j;
 
+	int *a;
+
+	if (min > max)
+		return (NULL);
+	j = max - (min + 1);
+	a = malloc(sizeof(int) * j);
+		if (a == NULL)
+			return (NULL);
+	for (i = 0; i < j; i++, min++)
+	{
+		a[i] = min;
+	}
+			return (a);
 }
