394f14d11ceb27869dbcd190942a4a85412f3844
Horluwatomee <fehinglobal14@gmail.com>
2022-10-07 16:43:05 -0700

    concatenates 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,53 +1,53 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * string_nconcat - function that concatenates two strings
+ * string_nconcat - concatenates two strings.
  * @s1: first string to be copied
  * @s2: second string to be copied
- * @n: number of bytes
+ * @n: amount of bytes allocated to the memory
  *
- * Return: pointer to s2 or NULL if it fails
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, k;
-
 	char *s;
 
+	unsigned int ts1, ts2, ts, i;
+
 	if (s1 == NULL)
-		i = 0;
-	else
 	{
-		for (i = 0; s1[i]; i++)
-			;
+		s1 = "";
 	}
 	if (s2 == NULL)
-	j = 0;
-	else
 	{
-		for (j = 0; s2[j]; j++)
-			;
+		s2 = "";
 	}
-	if (j > n)
+	for (ts1 = 0; s1[ts1] != '\0'; ts1++)
+		;
+	for (ts2 = 0; s2[ts2] != '\0'; ts2++)
+		;
+	if (n > ts2)
 	{
-		j = n;
-		s = malloc(sizeof(char) * (i + j + 1));
+		n = ts2;
 	}
+	ts = ts1 + n;
+	s = malloc(ts + 1);
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-	for (k = 0; k < i; k++)
-	{
-		s[k] = s1[k];
-	}
-	for (k = 0; k < j; k++)
-	{
-		s(k + i) = s2[k];
-		s[i + j] = '\0';
-	}
+	for (i = 0; i < ts; i++)
+		if (i < ts1)
+		{
+			s[i] = s1[i];
+		}
+		else
+		{
+			s[i] = s2[i - ts1];
+			s[i] = '\0';
+		}
 	return (s);
 }
