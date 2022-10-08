5dbe329902403adbecf408dcb89936dbb6c6d2ac
Horluwatomee <fehinglobal14@gmail.com>
2022-10-07 17:05:50 -0700

    concatenate 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -43,14 +43,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < ts; i++)
 		if (i < ts1)
-		{
 			s[i] = s1[i];
-		}
 		else
-		{
 			s[i] = s2[i - ts1];
 			s[i] = '\0';
-		}
 
-	return (s);
+			return (s);
 }
