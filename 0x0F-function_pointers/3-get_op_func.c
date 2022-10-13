9631ca91ec5b5adfeacadd46d062ed6cd80027be
Horluwatomee <fehinglobal14@gmail.com>
2022-10-13 07:30:43 -0700

    program that performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -26,9 +26,10 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i].op)
+
+	while (ops[i].op != NULL)
 	{
-		if (strcmp(ops[i].op, s) == 0)
+		if (*s == *ops[i].op)
 			return (ops[i].f);
 		i++;
 	}
