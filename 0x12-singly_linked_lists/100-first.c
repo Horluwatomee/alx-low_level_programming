76831ffcfceec6fb47a93addcf2d4c1e6af20500
Horluwatomee <fehinglobal14@gmail.com>
2022-10-21 03:24:37 -0700

    prints a sentence

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - prints a statement before execuring the main function
+ * function is executed
+ */
+
+void main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
