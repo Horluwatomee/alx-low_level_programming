e497ba6ccc3e92df9cc38a5d443193d8f4de127b
Horluwatomee <fehinglobal14@gmail.com>
2022-10-20 19:48:04 -0700

    prints all elements in a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,7 @@
 #include "list.h"
-#include <stdio>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * print_list - function that prints all elements in a list
@@ -10,11 +12,13 @@
 
 size_t print_list(const list_t *h)
 {
+	size_t s = 0;
+
 	if (h == NULL)
 		return (0);
-	if (h -> str)
-		printf("[%u] %s\n" h -> len, h -> str);
+	if (h->str)
+		printf("[%u] %s\n", h->len, h->str);
 	else
 		printf("[0] (nil)\n");
-	return (1 + print_list(h -> next));
+	return (1 + print_list(h->next));
 }
