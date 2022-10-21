f3feef42bddf48f9be629857c64e821a0b25a5dc
Horluwatomee <fehinglobal14@gmail.com>
2022-10-20 20:14:09 -0700

    prints all elements of a liast

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,11 +14,14 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	if (h == NULL)
-		return (0);
-	if (h->str)
-		printf("[%u] %s\n", h->len, h->str);
-	else
-		printf("[0] (nil)\n");
-	return (1 + print_list(h->next));
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+	return (s);
 }
