6bb4b4ed2ca2cd82fd0bf406964e67388778e5d5
Horluwatomee <fehinglobal14@gmail.com>
2022-10-21 04:34:16 -0700

    last node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,7 +13,7 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *node_end, *final;
+	list_t *node_end, *last;
 	int len;
 	char *dup_str;
 
@@ -38,11 +38,11 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = node_end;
 	else
 	{
-		final = *head;
-		while (final->next != NULL)
-			final = final->next;
-		final->next;
-		final->next = node_end;
+		last = *head;
+		while (last->next != NULL)
+		last = last->next;
+		last->next;
+		last->next = node_end;
 	}
 	return (*head);
 }
