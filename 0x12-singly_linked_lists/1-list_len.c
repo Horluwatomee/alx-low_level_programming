f3fd3ab3c9a554f0fdd91f4872a94d6798239964
Horluwatomee <fehinglobal14@gmail.com>
2022-10-20 20:34:26 -0700

    returns elements in a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * list_len - function that returns numbe rof elements in list_t
+ * @h: pointer to list_t
+ *
+ * Return: number of elements in h
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
