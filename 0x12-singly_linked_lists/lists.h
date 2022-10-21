b4a70265675e7b7954381e46c04bb8f5fa512fb2
Horluwatomee <fehinglobal14@gmail.com>
2022-10-20 20:03:12 -0700

    prints all elements in a list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stddef.h>
-#ifndef LIST_H
-#define LIST_H
 
 /**
  * struct list_s - singly linked list
@@ -13,11 +14,10 @@
 
 typedef struct list_s
 {
-	    char *str;
-	    unsigned int len;
-	    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
