fc4118cb609aac4f81d34dbaeb3f09527aa37e18
Horluwatomee <fehinglobal14@gmail.com>
2022-10-25 18:43:28 -0700

    print lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,11 +2,11 @@
 
 /**
  * print_listint - function that prints all the elements of a listint_t list.
+ *
  * @h: pointer to the first node
  *
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t num_nodes = 1;
