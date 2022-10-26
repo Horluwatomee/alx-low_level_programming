751f8a406d1abea88913b6f9e1969e522b21b02b
Horluwatomee <fehinglobal14@gmail.com>
2022-10-25 20:26:00 -0700

    free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,17 +4,15 @@
  * free_listint - function that frees the listint_t list
  * @head: head of the listint_t list
  *
- * Return: 0
  */
 
 void free_listint(listint_t *head)
 {
 	if (head == NULL)
-		return (0);
+		return;
 	/*free one node at a time*/
 	free_listint(head->next);
 
 	/*free the list*/
 	free(head);
-	return (0);
 }
