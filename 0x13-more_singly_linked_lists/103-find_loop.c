ab43623ae3812cb3470d263bc633d40bc1c37b4c
Horluwatomee <fehinglobal14@gmail.com>
2022-10-28 22:11:50 -0700

    find loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -13,28 +13,24 @@ listint_t *find_listint_loop(listint_t *head)
 	listint_t *p2 = head;
 	listint_t *prev = head;
 
-	while (head && p2 && p2->next)
+	if (head == NULL)
+		return (NULL);
+
+	while (prev && p2 && p2->next)
 	{
-		head = head->next;
+		prev = prev->next;
 		p2 = p2->next->next;
 
-		if (head == p2)
+		if (prev == p2)
 		{
-			head = prev;
-			prev =  p2;
-			while (1)
+			p2 = head;
+			while (p2 != prev)
 			{
-				p2 = prev;
-				while (p2->next != head && p2->next != prev)
-				{
-					p2 = p2->next;
-				}
-				if (p2->next == head)
-					break;
-				head = head->next;
+				p2 = p2->next;
+				prev = prev->next;
 			}
-			return (p2->next);
+			return (prev);
 		}
-		return (NULL);
 	}
+	return (NULL);
 }
