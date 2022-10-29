2b6cec1451b85a005431d181bdd46f2cdd9ed57a
Horluwatomee <fehinglobal14@gmail.com>
2022-10-28 19:55:52 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,16 +10,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i;
+	unsigned long int i;
 
-	if (n == 0 && index < 64)
-		return (0);
+	if (index > 64)
+		return (-1);
 
-	for (i = 0; i <= 63; n >>= 1, i++)
-	{
-		if (index == i)
-		{
-			return (n & 1);
-		}
-	}
+	i = n >> index;
+	return (i & 1);
 }
